55d329f3845190999be6afbbbd1d18e031a0feda
Timi Adewumi <nucleuscaesar@yahoo.com>
2021-09-23 18:59:29 +0100

    First Commit

---
+++ main.js
@@ -0,0 +1,20 @@
+visitorsNo = document.getElementById("count");
+
+const endpoint =
+  "https://t25llbvtaf.execute-api.us-east-1.amazonaws.com/default/LambdaFunction001";
+
+const getEndpoint = async () => {
+  try {
+    const response = await fetch(endpoint);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      visitorsNo.innerHTML = `${jsonResponse}`;
+      return;
+    }
+    throw new Error("Request failed!");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getEndpoint();
